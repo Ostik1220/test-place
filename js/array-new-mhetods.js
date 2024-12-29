2131c115d49512b2bfa8f8c6f279ccc83e46074a
Ostik <117195748+Ostik1220@users.noreply.github.com>
2024-12-29 19:28:05 +0200

    save

--- js/array-new-mhetods.js
+++ js/array-new-mhetods.js
@@ -1,7 +1,99 @@
-//метод forEach перебирає масив і робить щось з кожним елементом масива, нічого не поветає
-const friends = ["Yura", "Ruslan", "Marta"]
-friends.forEach(() => {
-    
-})
+
+// // метод forEach перебирає масив та робить щось з кожним елементом масиву, нічого не повертає
+// const friends = ["Andrey", "Stas", "Sasha"]
+// friends.forEach((friend, index, friendsArray) => {
+//     console.log(Друг ${friend}, має індекс ${index})
+// });
+
+// // Синтаксис перебираючих методів
+// // назваМасива.назваМетоду((елементМасиву, індексЕлементу, самМасив) => {Тут розписуємо що будемо робити з елементом масиву}) 
 
 
+// // метод map перебирає масив і з кожним елементом масиву робить те що ми вказали в тілі колбеку, і цей змінений елемент записує у новий масив
+// const updateFriends = friends.map((friend) => ${friend} мій друг);
+// console.log(updateFriends)
+
+
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//     { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//     { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+//   ];
+
+ //1
+//   const allPlayers = players.map((player) => player.name)
+//   console.log(allPlayers)
+//  //2
+//   const addPoints = players.map((player) => {
+//     const addedPoints = (player.points / 100) * 10 + player.points
+//    const updatePlayer = {...player, points: addedPoints }
+//    return updatePlayer
+//   }
+// )
+// console.log(addPoints)
+ //3
+
+// const addHours = players.map((player) => {
+//     const addedHourse = 10;
+//     const updatePlayer = {...player, timePlayed: player.timePlayed + addedHourse};
+//     return updatePlayer
+// }
+// )
+// console.log(addHours)
+
+
+// const playerToFind = players.fill((player) => player.id === 'player-3')
+// console.log(playerToFind)
+
+
+// function addHourse (hours, player) {
+//     return player.timePlayed + hours
+// }
+// console.log(addHourse(50, playerToFind))
+// console.log(players) 
+//третє не вдале
+const players = [
+    { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+    { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+    { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+    { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+    { id: 'player-5', name: 'Chelsy', timePlayed: 80, points: 48, online: true },
+  ];
+//4 Отримати масив всіх гравців онлайн
+ const findOnlinePlayers = players.filter((player) => player.online === true)
+ console.log(findOnlinePlayers)
+
+ //5 Отримати масив всіх гравців офлайн
+ const findOfflinePlayers = players.filter((player) => player.online === false)
+ console.log(findOfflinePlayers)
+
+ //6 масив всіх хардкорних гравців з часом більше 250 
+ const findHardcorePlayers = players.filter((player) => player.timePlayed >= 250) 
+ console.log(findHardcorePlayers)
+
+ //*
+
+ const findPlayerId= players.find((player) => player.id = "player-1")
+ console.log(findPlayerId)
+
+ const findPlayerName = players.find((player) => player.name = "Ajax")
+ console.log(findPlayerName)
+//-=-=-=----=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//метод fillter перебирає масив і перевіяє чи кожен елемнет з нього чи відповідає він умові яку ми вказалт в тілі колбека; повертає новий масив з елементами які пройшли перевірку(задовільняють умову)
+ //метод find працює подібно до fillter але вертає тільки перший підходящий елемент
+ //метод every перевіряє чи кожен елементз масиво вибирає вказаній умові якщо кожен то true a якщо не всі то false
+ const array = [15, 13, 5, 23, 45]
+const isNumEven = array.every((num) => num % 2 !== 0)
+
+ console.log(isNumEven)
+ // метод some перевряє чи хоча би 1 елемент в масиві правдивий умові
+ const isNumBiggerThanFourthy = array.some((number) => number > 40)
+ console.log(isNumBiggerThanFourthy)
+ //--=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-
+
+ const checkHourse = players.every((player) => player.timePlayed > 200)
+ console.log(checkHourse)
+ const checkOnline = players.every((player) => player.online)
+ console.log(checkOnline)
--- js/object-mhetods.js
+++ js/object-mhetods.js
@@ -121,16 +121,152 @@
 // console.log(userPaswordpassword)
  
 
-const course = {
-  title: "HTML+CSS+JS",
-  durationInMonth: 12,
-  students: ["Ivan", "Kiril", "Ostap", "Danya"],
-  school:{
-    title:"GoITeens",
-    country:"Ukraine",
-  }
+// const course = {
+//   title: "HTML+CSS+JS",
+//   durationInMonth: 12,
+//   students: ["Ivan", "Kiril", "Ostap", "Danya"],
+//   school:{
+//     title:"GoITeens",
+//     country:"Ukraine",
+//   }
+// }
+
+
+// const {title, durationInMonth, students, school:{title: schoolName, country}, cost = 500, logo} = course
+// console.log(title, schoolName, cost, students, logo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------------
+
+const musicLibrary = {
+  count: 2,
+  artists: [
+    {
+      name: 'The Beatles',
+      albums: [
+        {
+          title: 'Sgt. Pepper\'s Lonely Hearts Club Band',
+          year: 1967,
+          tracks: [
+            { title: 'With a Little Help from My Friends', duration: '2:44' },
+            { title: 'Lucy in the Sky with Diamonds', duration: '3:28' },
+            { title: 'A Day in the Life', duration: '5:33' }
+          ]
+        },
+        {
+          title: 'Abbey Road',
+          year: 1969,
+          tracks: [
+            { title: 'Come Together', duration: '4:19' },
+            { title: 'Something', duration: '3:01' },
+            { title: 'Here Comes the Sun', duration: '3:06' }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'Pink Floyd',
+      albums: [
+        {
+          title: 'The Wall',
+          year: 1979,
+          tracks: [
+            { title: 'Another Brick in the Wall, Part 2', duration: '3:59' },
+            { title: 'Comfortably Numb', duration: '6:23' },
+            { title: 'Hey You', duration: '4:40' }
+          ]
+        },
+        {
+          title: 'Dark Side of the Moon',
+          year: 1973,
+          tracks: [
+            { title: 'Speak to Me/Breathe', duration: '3:58' },
+            { title: 'Time', duration: '7:06' },
+            { title: 'Money', duration: '6:22' }
+          ]
+        }
+      ]
+    }        
+  ]
 }
 
+// const {count, artist:[firstGroupe: {theBeatles, firstAlbums: [albumOne: {titleOne, yearOne, tracksOne: [songOne: {trackTitleOne, durationOne}, songTwo: {trackTitleTwo, durationTwo}, songThree: {trackTitleThree, durationThree}]}, albumTwo: {titleTwo, yearTwo, tracksTwo:[songFour:{trackTitleFour, durationFour}, songFive:{trackTitleFive, durationFive}, songSix: {trackTitleSix, durationSix}]} ]}, secondGroup]} = musicLibrary
 
-const {title, durationInMonth, students, school:{title: schoolName, country}, cost = 500, logo} = course
-console.log(title, schoolName, cost, students, logo)
+
+
+// const {count, artist} = musicLibrary
+// const [firstGroupe, secondGroup] = artist
+// // 1
+// const {theBeatles, firstAlbums} = firstGroupe
+// const [albumOne, albumTwo] = firstAlbums
+
+// //1,1
+// const {titleOne, yearOne, tracksOne} = albumOne
+// const [songOne, songTwo, songThree] = tracksOne
+
+
+// //1,1,1
+// const {trackTitleOne, durationOne} = songOne
+// //1,1,2
+// const {trackTitleTwo, durationTwo} = songTwo
+// //1,1,3
+// const {trackTitleThree, durationThree} = songThree
+
+
+// //1,2
+// const {titleTwo, yearTwo, tracksTwo} = albumTwo
+// const [songFour, songFive, songSix] = tracksTwo
+
+
+// //1,2,1
+// const {trackTitleFour, durationFour} = songFour
+// //1,2,2
+// const {trackTitleFive, durationFive} = songFive
+// //1,2,3
+// const {trackTitleSix, durationSix} = songSix
+
+
+
+
+// //2
+// const {PinkFloyd, secondAlbums} = secondGroup
+// const [albumThree, albumFour] = secondAlbums
+// //2,1
+// const {titleThree, yearThree, tracksThree} = albumThree
+// const [songSeven, songEight, songNine] = tracksThree
+
+// //2,1,1
+// const {trackTitleSeven, durationSeven} = songSeven
+// //2,1,2
+// const {trackTitleEight, durationEight} = songEight
+// //2,1,3
+// const {trackTitleNine, durationNine} = songNine
+
+
+// //2,2
+// const {titleFour, yearFour, tracksFour} = albumFour
+// const [songTen, songEleven, songTwelve] = tracksFour
+
+
+// //2,2,1
+// const {trackTitleTen, durationTen} = songTen
+// //2,2,2
+// const {trackTitleEleven, durationEleven} = songEleven
+// //2,2,3
+// const {trackTitleTwelve, durationTwelve} = songTwelve
+
