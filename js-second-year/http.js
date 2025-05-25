93939502cf947f8c966411393de793941c42c3b7
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-05-25 18:56:38 +0300

    http

---
+++ js-second-year/http.js
@@ -0,0 +1,14 @@
+function listMaker(arr){
+const list = arr.map(object => {
+    const layout = `      <li>
+         <h1>name:${object.login}</h1>
+         <p>id:${object.id}</p>
+         <img src="${object.avatar_url}" alt="">
+      </li>`
+
+    return layout
+}).join("")
+return list
+}
+
+fetch("https://api.github.com/users").then(response => response.json()).then(data => document.querySelector(".http-list").innerHTML = listMaker(data))
